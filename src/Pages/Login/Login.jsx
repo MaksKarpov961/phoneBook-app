f0b74ea8bb75e9486428994e27dd0ca1fffa4662
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-12-15 22:51:04 +0200

    add img

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,11 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
+import phoneImage from "../../Img/4298345.png";
+import s from "./Login.module.css";
 
 const Login = () => {
   return (
     <div>
+      <img className={s.img} src={phoneImage} alt="Phone" />
       <LoginForm />
     </div>
   );
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,10 @@
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
-
+import phoneImage from "../../Img/4298345.png";
+import s from "./Register.module.css";
 const Register = () => {
   return (
     <div>
+      <img className={s.img} src={phoneImage} alt="Phone" />
       <RegisterForm />
     </div>
   );
